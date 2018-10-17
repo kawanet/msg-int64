faf9f141085dcc02c8efe7ad17a04ac3911a4bed
Yusuke Kawasaki <u-suke@kawa.net>
2018-10-17 22:37:08 +0900

    assert.strictEqual()

--- test/10.msg-int64.ts
+++ test/10.msg-int64.ts
@@ -10,33 +10,33 @@ const mtos = (msg: MsgInterface) => atos(msgToBuffer(msg));
 it("MsgInt64", () => {
     const msg = new MsgInt64(-1);
     assert(isMsg(msg));
-    assert.equal(msg, -1);
-    assert.equal(msg.msgpackLength, 9);
-    assert.equal(mtos(msg), "d3-ff-ff-ff-ff-ff-ff-ff-ff");
+    assert.strictEqual(+msg, -1);
+    assert.strictEqual(msg.msgpackLength, 9);
+    assert.strictEqual(mtos(msg), "d3-ff-ff-ff-ff-ff-ff-ff-ff");
 
-    assert.equal(mtos(new MsgInt64(0)), "d3-00-00-00-00-00-00-00-00");
-    assert.equal(mtos(new MsgInt64(4294967295)), "d3-00-00-00-00-ff-ff-ff-ff");
-    assert.equal(mtos(new MsgInt64(4294967296)), "d3-00-00-00-01-00-00-00-00");
-    assert.equal(mtos(new MsgInt64(Math.pow(2, 48))), "d3-00-01-00-00-00-00-00-00");
-    assert.equal(mtos(new MsgInt64(Math.pow(2, 62))), "d3-40-00-00-00-00-00-00-00");
+    assert.strictEqual(mtos(new MsgInt64(0)), "d3-00-00-00-00-00-00-00-00");
+    assert.strictEqual(mtos(new MsgInt64(4294967295)), "d3-00-00-00-00-ff-ff-ff-ff");
+    assert.strictEqual(mtos(new MsgInt64(4294967296)), "d3-00-00-00-01-00-00-00-00");
+    assert.strictEqual(mtos(new MsgInt64(Math.pow(2, 48))), "d3-00-01-00-00-00-00-00-00");
+    assert.strictEqual(mtos(new MsgInt64(Math.pow(2, 62))), "d3-40-00-00-00-00-00-00-00");
 
-    assert.equal(+new MsgInt64([0, 0, 0, 0, 0, 0, 0, 1]), 1);
-    assert.equal(+new MsgInt64([0, 0, 0, 1, 0, 0, 0, 0]), 4294967296);
+    assert.strictEqual(+new MsgInt64([0, 0, 0, 0, 0, 0, 0, 1]), 1);
+    assert.strictEqual(+new MsgInt64([0, 0, 0, 1, 0, 0, 0, 0]), 4294967296);
 });
 
 it("MsgUInt64", () => {
     const msg = new MsgUInt64(1);
     assert(isMsg(msg));
-    assert.equal(msg, 1);
-    assert.equal(msg.msgpackLength, 9);
-    assert.equal(mtos(msg), "cf-00-00-00-00-00-00-00-01");
+    assert.strictEqual(+msg, 1);
+    assert.strictEqual(msg.msgpackLength, 9);
+    assert.strictEqual(mtos(msg), "cf-00-00-00-00-00-00-00-01");
 
-    assert.equal(mtos(new MsgUInt64(0)), "cf-00-00-00-00-00-00-00-00");
-    assert.equal(mtos(new MsgUInt64(4294967295)), "cf-00-00-00-00-ff-ff-ff-ff");
-    assert.equal(mtos(new MsgUInt64(4294967296)), "cf-00-00-00-01-00-00-00-00");
-    assert.equal(mtos(new MsgUInt64(Math.pow(2, 48))), "cf-00-01-00-00-00-00-00-00");
-    assert.equal(mtos(new MsgUInt64(Math.pow(2, 63))), "cf-80-00-00-00-00-00-00-00");
+    assert.strictEqual(mtos(new MsgUInt64(0)), "cf-00-00-00-00-00-00-00-00");
+    assert.strictEqual(mtos(new MsgUInt64(4294967295)), "cf-00-00-00-00-ff-ff-ff-ff");
+    assert.strictEqual(mtos(new MsgUInt64(4294967296)), "cf-00-00-00-01-00-00-00-00");
+    assert.strictEqual(mtos(new MsgUInt64(Math.pow(2, 48))), "cf-00-01-00-00-00-00-00-00");
+    assert.strictEqual(mtos(new MsgUInt64(Math.pow(2, 63))), "cf-80-00-00-00-00-00-00-00");
 
-    assert.equal(+new MsgUInt64([0, 0, 0, 0, 0, 0, 0, 1]), 1);
-    assert.equal(+new MsgUInt64([0, 0, 0, 1, 0, 0, 0, 0]), 4294967296);
+    assert.strictEqual(+new MsgUInt64([0, 0, 0, 0, 0, 0, 0, 1]), 1);
+    assert.strictEqual(+new MsgUInt64([0, 0, 0, 1, 0, 0, 0, 0]), 4294967296);
 });
